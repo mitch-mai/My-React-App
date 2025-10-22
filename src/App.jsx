da551876f841c984d3564f1d1a8b9110f5e403ba
mitch-mai <mitch.mai@volkert.com>
2025-10-22 14:45:32 -0500

    Remove unused button components and clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Button from './components/Buttons/Button';
 import CounterButton from './components/Buttons/CounterButton';
+/*import Button from './components/Buttons/Button';
 import HoverButton from './components/Buttons/HoverButton';
-import ImgButton from './components/Buttons/ImgButton';
+import ImgButton from './components/Buttons/ImgButton'; */
 
 export default function App() {
-  const handleClick = () => {
-    alert('Basic button clicked!');
-  };
 
   return (
     <div className="min-h-screen bg-black text-white flex flex-col">
@@ -18,22 +15,8 @@ export default function App() {
       <main className="flex-1 flex flex-col items-center justify-center gap-8">
         <h1 className="text-4xl font-bold">Welcome Page</h1>
 
-        {/* Button row */}
-        <div className="flex items-center justify-center gap-6">
-          {/* Basic button (center) */}
-          <Button onClick={handleClick}>Bonk</Button>
-
           {/* Counter button (side) */}
           <CounterButton />
-
-          {/* Hover button (side) */}
-          <HoverButton label="Hover me" />
-
-          {/* Image button (side) */
-          <ImgButton imgSrc="/path/to/image.png" altText="Example Image Button" />
-          }
-
-        </div>
       </main>
 
       <Footer />
